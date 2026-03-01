1b183004c7e5141bdeaea2badfde0074ab4778a5
YevhenKharchenko <evgenvalerych@gmail.com>
2026-03-01 11:15:41 +0200

    Add categories and progress sections

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './js/header.js';
+import './js/categories.js';
 import './js/experience.js';
-import './js/illusions.js';
 import './js/reviews.js';
 import './js/faq.js';
 import './js/banner.js';
